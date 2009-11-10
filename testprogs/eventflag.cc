777f97e3d8bf5c690b611bbf51fefee1f9c91b88
Helge Bahmann <hcb@chaoticmind.net>
2009-11-10 15:15:29 +0100

    Add unit test for eventflag (most notably pipe_eventflag)

---
+++ testprogs/eventflag.cc
@@ -0,0 +1,42 @@
+/* -*- C++ -*-
+ * (c) 2009 Helge Bahmann <hcb@chaoticmind.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * Refer to the file "COPYING" for details.
+ */
+
+#include <boost/bind.hpp>
+
+#define private public
+#define protected public
+
+#include "tests.h"
+
+#include <tscb/ioready>
+#include <pthread.h>
+
+void test_pipe_eventflag(void)
+{
+	tscb::pipe_eventflag e;
+	
+	ASSERT(e.flagged==0);
+	e.set();
+	ASSERT(e.flagged==1);
+	e.clear();
+	ASSERT(e.flagged==0);
+	
+	e.start_waiting();
+	ASSERT(e.waiting==1);
+	e.stop_waiting();
+	ASSERT(e.waiting==0);
+	
+	e.set();
+	ASSERT(e.flagged==1);
+	e.clear();
+}
+
+main()
+{
+	test_pipe_eventflag();
+}
