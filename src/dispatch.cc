65e42c74331a29fb7cde25ba30870ddd78cfb925
Helge Bahmann <hcb@chaoticmind.net>
2009-11-20 21:44:19 +0100

    Compound dispatcher class

--- src/dispatch.cc
+++ src/dispatch.cc
@@ -39,4 +39,82 @@ namespace tscb {
 		} else io->dispatch(0);
 	}
 	
+	workqueue_service::~workqueue_service(void) throw()
+	{
+	}
+	
+	posix_event_service::~posix_event_service(void) throw()
+	{
+	}
+	
+	posix_event_dispatcher::posix_event_dispatcher(void)
+		throw(std::bad_alloc, std::runtime_error)
+		: io(create_ioready_dispatcher()),
+		flag(io->get_eventflag()),
+		timer(flag)
+	{
+	}
+	
+	posix_event_dispatcher::~posix_event_dispatcher(void) throw()
+	{
+		delete io;
+	}
+	
+	void
+	posix_event_dispatcher::post(const boost::function<void(void)> &function) throw(std::bad_alloc)
+	{
+		workqueue_lock.lock();
+		workqueue.push_back(function);
+		workqueue_lock.unlock();
+		flag->set();
+	}
+		
+	void posix_event_dispatcher::register_timer(timer_callback *ptr) throw()
+	{
+		timer.register_timer(ptr);
+	}
+	
+	void posix_event_dispatcher::unregister_timer(timer_callback *t) throw()
+	{
+		timer.unregister_timer(t);
+	}
+	
+	void
+	posix_event_dispatcher::register_ioready_callback(ioready_callback *l) throw(std::bad_alloc)
+	{
+		io->register_ioready_callback(l);
+	}
+	
+	void
+	posix_event_dispatcher::unregister_ioready_callback(ioready_callback *e) throw()
+	{
+		io->unregister_ioready_callback(e);
+	}
+	
+	void
+	posix_event_dispatcher::modify_ioready_callback(ioready_callback *e, ioready_events event_mask) throw()
+	{
+		io->modify_ioready_callback(e, event_mask);
+	}
+	
+	eventflag *
+	posix_event_dispatcher::get_eventflag(void) throw(std::bad_alloc)
+	{
+		return flag;
+	}
+	void
+	posix_event_dispatcher::dispatch(void)
+	{
+		if (__builtin_expect(!workqueue.empty(), 0)) {
+			workitems items;
+			workqueue_lock.lock();
+			items.swap(workqueue);
+			workqueue_lock.unlock();
+			
+			for(workitems::const_iterator i=items.begin(); i!=items.end(); ++i)
+				(*i)();
+		}
+		tscb::dispatch(&timer, io);
+	}
+	
 };
